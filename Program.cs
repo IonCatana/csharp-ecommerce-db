b58c5d6cad90f86cff3cb7a5334510b9f4aa7be4
IonCatana <catana.ion17@yahoo.it>
2022-07-12 16:58:21 +0200

    add 3 product in db

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using (EccomerceContext db = new EccomerceContext())
+{
+    //Inserimento prodotti
+    Product product1 = new Product { Name = "Samsung" };
+    Product product2 = new Product { Name = "Apple" };
+    Product product3 = new Product { Name = "Windows" };
+    product1.Description = "Lorem ipsum";
+    product2.Description = "Lorem ipsum";
+    product3.Description = "Lorem ipsum";
+    db.Add(product1);
+    db.Add(product2);
+    db.Add(product3);
+    db.SaveChanges();
+}
